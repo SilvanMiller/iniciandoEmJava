f10077755151655f68050b128ad2a2295878ad72
Silvan Miller <silvanmiller@gmail.com>
2023-03-22 07:26:06 -0300

    versionando minhas primeiras linhas em java

---
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*main é um metodo auto executavel*/
+	public static void main(String[] args) {
+		
+		/*imprimir/escrever no console*/
+		System.out.println("Meu primeiro codigo em Java - Silvan Miller");
+		System.out.println("Java Orientado a Objeto");
+	}
+
+}
